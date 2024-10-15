3e358a3cc2c90c3823ae826a79f0de9bc61ee270
WinnerOK <danman339@gmail.com>
2024-10-15 21:06:29 +0300

    draft implementation of backend

--- shell/crates/shell/src/backend/mod.rs
+++ shell/crates/shell/src/backend/mod.rs
@@ -1,6 +1,6 @@
 use std::{
     error::Error,
-    io::{Read, Write},
+    process::{Command as ProcessCommand, Output, Stdio},
 };
 
 use crate::ir::Command;
@@ -8,11 +8,108 @@ use crate::ir::Command;
 pub struct Backend;
 
 impl Backend {
-    pub fn exec<W, R>(&mut self, command: Command, r: R, w: W) -> Result<(), Box<dyn Error>>
-    where
-        R: Read,
-        W: Write,
+    pub fn exec(
+        &self,
+        command: Command,
+        // r: R, w: W,
+    ) -> Result<(), Box<dyn Error>>
+// where
+    //     R: Read,
+    //     W: Write,
     {
-        unimplemented!()
+        self.exec_with_io(
+            command,
+            Stdio::inherit(),
+            Stdio::inherit(),
+            Stdio::inherit(),
+        )
+        .map(|_| ())
+    }
+
+    fn exec_with_io(
+        &self,
+        command: Command,
+        stdin: Stdio,
+        stdout: Stdio,
+        stderr: Stdio,
+    ) -> Result<Option<Output>, Box<dyn Error>> {
+        match command {
+            Command::PipeCommand(_pipe_command) => unimplemented!(),
+            Command::CallCommand(call_command) => {
+                let mut process_command = ProcessCommand::new(&call_command.argv[0]);
+                process_command
+                    .args(&call_command.argv[1..])
+                    .stdin(stdin)
+                    .stdout(stdout)
+                    .stderr(stderr)
+                    .envs(call_command.envs);
+
+                let output = process_command.spawn()?.wait_with_output()?;
+                return Ok(Some(output));
+            }
+            Command::ExitCommand => {
+                return Ok(None);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ir::CallCommand;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_call_command_stdout() -> Result<(), Box<dyn Error>> {
+        let backend = Backend;
+        let test_str = "Hello, world!";
+        let command = Command::CallCommand(CallCommand {
+            envs: HashMap::new(),
+            argv: vec!["echo".to_string(), test_str.to_string()],
+        });
+
+        let execution_result =
+            backend.exec_with_io(command, Stdio::piped(), Stdio::piped(), Stdio::piped())?;
+        let Some(output) = execution_result else {
+            panic!("Expected to spawn some and get its output");
+        };
+
+        assert_eq!(Some(0), output.status.code());
+        assert_eq!(
+            String::from_utf8_lossy(&output.stdout),
+            format!("{}\n", test_str)
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_failing_command_does_not_fail_shell() -> Result<(), Box<dyn Error>> {
+        let backend = Backend;
+        let command = Command::CallCommand(CallCommand {
+            envs: HashMap::new(),
+            argv: vec!["sh".to_string(), "-c".to_string(), r#"exit 5"#.to_string()],
+        });
+
+        let execution_result =
+            backend.exec_with_io(command, Stdio::piped(), Stdio::piped(), Stdio::piped())?;
+        let Some(output) = execution_result else {
+            panic!("Expected to spawn some and get its output");
+        };
+
+        assert_eq!(Some(5), output.status.code());
+        Ok(())
+    }
+
+    #[test]
+    fn test_exit_just_returns() -> Result<(), Box<dyn Error>> {
+        let backend = Backend;
+        let command = Command::ExitCommand;
+
+        let execution_result =
+            backend.exec_with_io(command, Stdio::piped(), Stdio::piped(), Stdio::piped())?;
+        assert_eq!(None, execution_result);
+        Ok(())
     }
 }
--- shell/crates/shell/src/main.rs
+++ shell/crates/shell/src/main.rs
@@ -1,7 +1,21 @@
+use backend::Backend;
+use ir::{CallCommand, Command, PipeCommand};
+
 mod backend;
 mod frontend;
 mod ir;
 
 fn main() {
-    println!("Hello, world!");
+    /* Simple example of running
+    use std::collections::HashMap;
+
+    let call_command = Command::CallCommand(CallCommand {
+        envs: HashMap::new(),
+        argv: vec!["echo".to_string(), "Hello, world!".to_string()],
+    });
+
+
+    let mut executor = Backend;
+    executor.exec(call_command).expect("Expeceted ok");
+    */
 }
