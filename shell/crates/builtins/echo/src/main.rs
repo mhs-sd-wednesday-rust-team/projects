3c1f5cedf9e00e8bfeb193043f60da1818681bc4
achains <56088401+achains@users.noreply.github.com>
2024-10-15 17:48:02 +0300

    feat: cat echo pwd

--- shell/crates/builtins/echo/src/main.rs
+++ shell/crates/builtins/echo/src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    content: Vec<String>,
+
+    #[arg(short = 'n')]
+    remove_trailing_newline: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let mut output = args.content.join(" ");
+    (!args.remove_trailing_newline).then(|| output += "\n");
+    print!("{output}")
 }
